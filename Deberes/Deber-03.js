608191603c8fc725fa7786131f0a31cb54079de3
Jessica Ayala <jessydayan3@gmail.com>
2020-05-12 10:59:01 -0500

    repaso del CRUD

--- Deberes/Deber-03.js
+++ Deberes/Deber-03.js
@@ -1,55 +1,55 @@
 var arreglo = [
-    {nombre: 'kevin',
+    {nombre: 'kevin',       //se crea nuevo array con datos personales
     edad: 25,
     fecha: new Date('1995/02/24'),
     telf: "0999999999"}
 ]
 
-function crearUsuario (arreglo,usuario,cb){
-    if (arreglo == usuario){
-        cb({
-            mensaje: 'usuario existe',
-            arreglo
+function crearUsuario (arreglo,usuario,cb){        //funcion para crear un usuario
+    if (arreglo == usuario){                           // compara si el usuario que ingreso es igual al array
+        cb({                                          // guarda informacion al callback
+            mensaje: 'usuario existe',                 //envia mensaje "usuario existe"
+            arreglo                                    // envio arreglo
         })
-    }else{
-        arreglo.push(usuario);
+    }else{                                             // caso contrario a la condicion
+        arreglo.push(usuario);                               // agrega el usuario al array
         cb({
-            mensaje: 'usuario ingresado correctamente',
+            mensaje: 'usuario ingresado correctamente',      
             arreglo
         })
     }
 }
 
-var usuario = {nombre: 'jessy',
+var usuario = {nombre: 'jessy',                           // crea el array de usuario
 edad: 22,
 fecha: new Date('1997/10/20'),
 telf: "0999999998"}
 
-crearUsuario(arreglo,usuario,(respuestaMensaje) => {
-    console.log(respuestaMensaje);
+crearUsuario(arreglo,usuario,(respuestaMensaje) => {       // llama a la funcion "crear usuario" y genera una funcion del callback
+    console.log(respuestaMensaje);                          // imprime informacion del callback
 })
 
-function editarUsuario(arreglo,usuario,ind,cb){
-    arreglo[ind]=usuario;
+function editarUsuario(arreglo,usuario,ind,cb){             //  Crea la funcion editar usuario 
+    arreglo[ind]=usuario;                                    // reemplaza en el array por el usuario
     cb({
         mensaje: 'usuario a sido cambiado',
         arreglo
     })
 }
 
-var usuario2 = {nombre: 'maria',
+var usuario2 = {nombre: 'maria',                               // crea el array usuario 2
 edad: 28,
 fecha: new Date('1992/08/10'),
 telf: "0999997998"}
 
-editarUsuario(arreglo,usuario2,1,(respuestaMensaje) => {
-    console.log(respuestaMensaje);
+editarUsuario(arreglo,usuario2,1,(respuestaMensaje) => {        // llama a la funcion editar usuario y genera la funcion callback
+    console.log(respuestaMensaje);                               // imprime la informacion del callback 
 })
 
-function eliminarUsuario(arreglo,ind,cb){
-    var i = arreglo.indexOf( ind );
-    if ( i !== -1 ) {
-        arreglo.splice( i, 1 );
+function eliminarUsuario(arreglo,ind,cb){                        // crea la funcion eliminar usuario
+    var i = arreglo.indexOf( ind );                              // en la variable i obtengo el array del indice 
+    if ( i !== -1 ) {                                             // si el arreglo existe mandara un valor positivo, por el cual pongo un if para ver si existe ese indice
+        arreglo.splice( i, 1 );                                  // con splice elimino el array de aquel indice
         cb({
             mensaje: 'usuario a sido eliminado',
             arreglo
@@ -63,21 +63,21 @@ function eliminarUsuario(arreglo,ind,cb){
     
 }
 
-eliminarUsuario(arreglo,1,(respuestaMensaje) => {
-    console.log(respuestaMensaje);
+eliminarUsuario(arreglo,1,(respuestaMensaje) => {                // llamo a la funcion eliminar usuario con su callback 
+    console.log(respuestaMensaje);                               // imprime la informacion del callback
 })
 
-function listarUsuario(arreglo, cb) {
-    arreglo
-        .forEach(
-            usuario => {
-                console.log(usuario);
+function listarUsuario(arreglo, cb) {                          // crea la funcion listar usuario
+    arreglo                                                  // dentro del arreglo 
+        .forEach(                                            // recorre ese arreglo con forEach
+            usuario => {                                     // el item del arreglo se llama usuario 
+                console.log(usuario);                        // imprime el item
             });
-    cb({
+    cb({                                                           // guarda en el callback un mensaje
         mensaje: 'lista de usuario'
     })
 }
 
-listarUsuario(arreglo, (respuestaMensaje) => {
-    console.log(respuestaMensaje);
+listarUsuario(arreglo, (respuestaMensaje) => {                   // llama a la funcion listar usuario 
+    console.log(respuestaMensaje);                                // imprime informacion del callback
 })
