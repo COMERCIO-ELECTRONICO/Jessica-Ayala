06390dd2a417e300329f9d2b4f8998a09cafc530
Jessica-Ayala <Jessydayan3@gmail.com>
2020-05-16 03:20:09 -0500

    Proyecto Angular y Deber

---
+++ proyecto-angularpr/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PadreComponent } from './padre/padre.component';
+import { HijoComponent } from './hijo/hijo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PadreComponent,
+    HijoComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
